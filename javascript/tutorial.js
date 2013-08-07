5b8b72070f61d25d90f64aaaa01ad878597d824c
Jeffrey Gunderson <jeffrey.gunderson@gmail.com>
2013-08-07 15:23:51 -0600

    updated tutorial.js to include ajax error

--- javascript/tutorial.js
+++ javascript/tutorial.js
@@ -2,7 +2,11 @@ function showContent(url){
 	$.ajax(url, {
 		success: function(response) {
 			$("#info_container").html(response).fadeIn();	
-		}
+		},
+		error: function(request, error, errorText) {
+			$("#info_container").html("<p>There was an error getting this tutorial.  Error: " + error +", Error Message "+ errorText +"</p>");
+		},
+		timeout: 3000
 	});
 }
 $(document).ready( function() {
