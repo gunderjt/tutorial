182965ed3416d180ae8973dd5dabff37cf799f25
Jeffrey Gunderson <jeffrey.gunderson@gmail.com>
2013-08-09 14:08:19 -0600

    Changed the css to include dynamic Tutdesc location Improved tutorial.js to not include global variables tutorial.js now has smooth transitions All tutHTMLs have instructions hardcoded into the HTML Main.html has less instructions

--- javascript/tutorial.js
+++ javascript/tutorial.js
@@ -8,12 +8,52 @@ function showContent(url){
 		},
 		timeout: 3000
 	});
+	getNsetState(0);
+}
+
+function animateTut(){
+	
+	if(getNsetState() == 0){
+    	$(".tutImage").animate({
+   				opacity: "0",
+    			width: "0%",
+    			height: "toggle"},
+    			{duration: 400, queue: false});
+		$(".tutDesc").animate({
+				width:"99%"},
+				{duration: 400, queue: false});
+		getNsetState(1);
+		}
+	else{
+    	$(".tutImage").animate({
+   				opacity: "1",
+    			width: "59%",
+    			height: "toggle"},
+    			{duration: 400, queue: false});
+		$(".tutDesc").animate({
+				width:"40%"},
+				{duration: 400, queue: false});
+		getNsetState(0);	
+	}
+}
+
+function getNsetState(value){
+	//initialize the state of the program
+	if(typeof getNsetState.state == 'undefined'){
+		getNsetState.state = 0;
+	}
+	//test if value was passed in
+	if (typeof value == 'undefined'){
+		return getNsetState.state;
+	}
+	else{
+		return (getNsetState.state = value);
+	}
 }
 $(document).ready( function() {
+	window.JTGstate = 0;
 	$(".showContent").on('click', function () {	
 		showContent($(this).data('url'));
 	});
-	$("#Hshot").on('click', function() {
-		$(".tutImage").fadeToggle(600);
-	});
+	$("#Hshot").on('click', animateTut);
 });
